285746d9da6c2518c4caf09daf7338510b0d9c37
Anthony Potts <71612698+anthonypotts@users.noreply.github.com>
2020-10-21 20:20:16 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,34 @@
 // Link to HTML //
 var generateBtn = document.querySelector("#generate");
 
-
 // VARIABLES //
 var charLower = "abcdefghijklmnopqrstuvwxyz";
 var charUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numRandom = "1234567890";
 var specialChar = "!@#$%^&*()-_=+`~<>,.;:/?[{]}";
 
-var newPassword = ""
+var newPassword = "";
 var compiledChar;
 var password = {
-  charLower: true,
-  charUpper: true,
-  numRandom: true,
-  specialChar: true,
+  charLowers: true,
+  charUppers: true,
+  numRandoms: true,
+  specialChars: true,
   reset: function() {
-    this.charLower = true;
-    this.charUpper = true;
-    this.numRandom = true;
-    this.specialChar = true;
+    this.charLowers = true;
+    this.charUppers = true;
+    this.numRandoms = true;
+    this.specialChars = true;
   }
 }
-
 
 // ASSIGNMENT CODE //
 function generatePassword() {
   password.reset();
 
-  var pwdLength = window.prompt("How many characters would you like your password to contain? Please pick a number between 8 and 128.");
-  
+  var pwdLength = parseInt(prompt("How many characters would you like your password to contain? Please pick a number between 8 and 128."));
     // DETERMINE IF NUMBER IS BETWEEN 8-128 AND/OR EXISTS //
+    debugger;
     if (!pwdLength) {
       window.alert("You need to enter a NUMBER and it must be between 8 and 128. Please try again.");
     }else if (pwdLength <8 || pwdLength > 128) {
@@ -39,50 +37,48 @@ function generatePassword() {
 
     // IF VALID NUMBER EXISTS, CONTINUE AND GATHER USER PREFERENCES //
     }else{
-      password.charLower = confirm("Please select 'OK' if you would like 'lower-case letters' in your password");
-      password.charUpper = confirm("Please select 'OK' if you would like 'UPPER-CASE LETTERS' in your password");
-      password.numRandom = confirm("Please select 'OK' if you would like 'numbers' in your password");
-      password.specialChar = confirm("Please select 'OK' if you would like 'special characters' in your passsord");
+      password.charLowers = confirm("Please select 'OK' if you would like 'lower-case letters' in your password");
+      password.charUppers = confirm("Please select 'OK' if you would like 'UPPER-CASE LETTERS' in your password");
+      password.numRandoms = confirm("Please select 'OK' if you would like 'numbers' in your password");
+      password.specialChars = confirm("Please select 'OK' if you would like 'special characters' in your passsord");
     };
 
-    if(!password.charLower && !password.charUpper && !password.numRandom && !password.specialChar){
+    if(!password.charLowers && !password.charUppers && !password.numRandoms && !password.specialChars){
       window.alert("Please select at least one criteria")
       generatePassword();
     }
 
-    if(password.charLower === true){
+    if(password.charLowers === true){
       compiledChar += charLower;
     }
 
-    if(password.charUpper === true){
+    if(password.charUppers === true){
       compiledChar += charUpper;
     }
 
-    if(password.numRandom === true){
+    if(password.numRandoms === true){
       compiledChar += numRandom;
     }
 
-    if(password.specialChar === true){
+    if(password.specialChars === true){
       compiledChar += specialChar;
     }
 
   for (i = 0; i < pwdLength; i++){
   var char = Math.floor(Math.random() * compiledChar.length);
-  newPassword += pwdLength.charAt(char, char +1);
-  return newPassword;
+  newPassword += compiledChar.charAt(char, char +1);
   }
-    
-  window.alert("Your new password is" + newPassword);
+  return newPassword;
 };
+
 
 // WRITE PASSWORD IN  #password INPUT //
 function writePassword() {
-  var password = generatePassword();
+  let newPassword = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = newPassword;
 };
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
